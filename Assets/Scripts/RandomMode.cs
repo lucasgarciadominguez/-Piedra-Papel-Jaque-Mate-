f171b065ffce5aff99b7866b6bcc6aab9fdb1181
locosporelrugby <lucaserious002@gmail.com>
2023-06-05 14:17:12 +0200

    Shader acabado para peon negro y blanco

--- Assets/Scripts/RandomMode.cs
+++ Assets/Scripts/RandomMode.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using Image = UnityEngine.UI.Image;
 
 public enum RandomChoices
 {
@@ -39,14 +42,23 @@ public class RandomMode : MonoBehaviour
     [SerializeField]
     EatPiece eatPiece;
     bool firstTime = false;
+    bool check = false;
+    [SerializeField]
+    Image imagePlayer1Piece;
+    [SerializeField]
+    Image imagePlayer2Piece;
     private void Awake()
     {
     }
     public void StartRandomMode(GameObject actualTile, Cell otherTile,Team teamTurn)
     {
+        ChangeColor();
+
         Sprite piece = actualTile.GetComponent<SpriteRenderer>().sprite;
+        Material mat1= actualTile.GetComponent<SpriteRenderer>().material;
         Sprite piece2 = otherTile.currentPiece.GetComponent<SpriteRenderer>().sprite;
-        managerUI.PassInformationRandomMode(piece, piece2);
+        Material mat2 = otherTile.currentPiece.GetComponent<SpriteRenderer>().material;
+        managerUI.PassInformationRandomMode(piece, piece2, mat1, mat2);
         managerUI.SelectTurns(false);
     }
 
@@ -93,6 +105,8 @@ public class RandomMode : MonoBehaviour
             managerUI.SelectTurns(true);
             if (firstTime)
             {
+                ChangeColor();
+                Debug.Log("h");
                 animatorPlayer1Cards.SetTrigger("ChangeTurnPlayer1");
                 animatorPlayer2Cards.SetTrigger("ChangeTurnPlayer2");
                 animatorPlayer1Piece.SetTrigger("ChangeTurnPlayer1Piece");
@@ -114,6 +128,43 @@ public class RandomMode : MonoBehaviour
             FinishRandomMode();
         }
 
+
+    }
+    void ChangeColor()
+    {
+        if (check)
+        {
+            Color color = imagePlayer1Piece.material.GetColor("_OutlineColor");
+            float factor = Mathf.Pow(2, 1);
+
+            color = new Color(color.r * factor, color.g * factor, color.b * factor, color.a);
+            imagePlayer1Piece.material.SetColor("_OutlineColor", color);
+
+            Color color2 = imagePlayer2Piece.material.GetColor("_OutlineColor");
+            float factor2 = Mathf.Pow(2, 1);
+
+
+            color2 = new Color(color2.r * factor2, color2.g * factor2, color2.b * factor2, color2.a);
+            imagePlayer2Piece.material.SetColor("_OutlineColor", color2);
+            check = false;
+        }
+        else
+        {
+            Color color = Color.yellow;
+            float factor = Mathf.Pow(2, 1);
+            Debug.Log(factor);
+
+            color = new Color(color.r * factor, color.g * factor, color.b * factor, color.a);
+            imagePlayer1Piece.material.SetColor("_OutlineColor", color);
+
+            Color color2 = Color.yellow;
+            float factor2 = Mathf.Pow(2, -10f);
+            Debug.Log(factor2);
+
+            color2 = new Color(color2.r * factor2, color2.g * factor2, color2.b * factor2, color2.a);
+            imagePlayer2Piece.material.SetColor("_OutlineColor", color2);
+            check = true;
+        }
 
     }
     void FinishRandomMode()
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -24,10 +24,12 @@ public class UIManager : MonoBehaviour
         uiText.SetActive(true);
     }
 
-    public void PassInformationRandomMode(Sprite spritePlayer1, Sprite spritePlayer2)
+    public void PassInformationRandomMode(Sprite spritePlayer1, Sprite spritePlayer2,Material materialPlayer1, Material materialPlayer2)
     {
         player1Piece.sprite = spritePlayer1;
+        player1Piece.material = materialPlayer1;
         player2Piece.sprite = spritePlayer2;
+        player2Piece.material = materialPlayer2;
     }
     public void SelectTurns(bool changeTurns)
     {
